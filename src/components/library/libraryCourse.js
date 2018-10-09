bb00e01b35feeae56c24843d81db60967418c1c2
Jaxsonman <42976152+Jaxsonman@users.noreply.github.com>
2018-10-09 09:26:24 -0600

    more grid design

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import Icon from '../icon';
+import Arrow from '../arrow';
 
 class LibraryCourse extends Component {
     render () {
         return (
             <div className="library-course">
+              <div className="library-course__title-arrow">
                 <label className="library-course_title">Problem Solving</label>
                 { Icon('fas fa-check', 'library-course__icon') }
-        
+              </div>
+                <Arrow className="library-course__arrow"/>
 
                 <div className="library-course_description">
                     <label>Course Description</label>
